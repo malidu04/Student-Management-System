a3b14b55516741ac831d4aa5838ef4b372cc3331
Malidu Pahasara <malidupahasara04@gmail.com>
2024-10-31 01:28:45 +0530

    student schema

---
+++ backend/models/studentSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const studentSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String
+    }
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('Student', studentSchema)
